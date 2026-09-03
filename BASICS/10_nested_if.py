f28a9c1a0b1f3461cb9327ea46d9a4af4fb140fd
Muhammad Aizaz Khan <aizazawan533@gmail.com>
2026-09-04 03:25:51 +0500

    Create 10_nested_if.py

---
+++ BASICS/10_nested_if.py
@@ -0,0 +1,10 @@
+age=int(input("Enter you age :"))
+if(age>=18) :
+ cnic=input("do you have cnic :")
+ if "cnic" == "yes" :
+    print("you can vote")
+ else :
+   print("you need cinc ")
+
+else :
+  print("you cannot vote")
